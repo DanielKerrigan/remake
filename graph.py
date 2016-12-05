3a2e22bf2500b045b66156a3cbbfa8dee464d036
Royce Branning <rbrannin@nd.edu>
2016-12-05 11:50:34 -0500

    topological sort method added to graph class

--- graph.py
+++ graph.py
@@ -2,3 +2,18 @@ class Graph(object):
     def __init__(self):
         self.edges = {}
         self.degrees = {}
+
+    def topological_sort(self);
+        frontier = []
+        results = []
+        for v,d in self.degrees:
+            if d == 0:
+                frontier.push(v)
+        while not frontier.empty():
+            v = frontier.pop()
+            results.append(v)
+            for u in self.edges[v]:
+                self.degrees[u] -= 1
+                if self.degrees[u] == 0:
+                    frontier.push(u)
+        return results
