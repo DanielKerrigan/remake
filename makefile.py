9164d3d40133d0afd70f25abd50695dc9149834f
Royce Branning <rbrannin@nd.edu>
2016-12-06 15:03:31 -0500

    coded and commented makefile parse method, have not tested

--- makefile.py
+++ makefile.py
@@ -9,24 +9,32 @@ class Makefile(object):
     def parse_makefile(self):
         # only handles dependencies for now
         lines = self.file_name.read().splitlines()
+
         # enumerate returns a tuple with the index as well as the content
         for line_num, line in enumerate(lines):
             if line.find(':') == -1:
                 continue
             line = line.split(':')
+            # target is the variable that preceds the :
             target = line[0]
-            line[1] = line.strip('\t')
+            line[1] = line[1].strip('\t')
+            # sources are the variables after the :
             sources = line[1].split(' ')
-           
-            #LEFT OFF HERE 
-            if graph.degrees
-            graph.degrees[target] += len(sources)
+          
+            # update the degrees dictionary for the target 
+            if target in self.graph.degrees:
+                self.graph.degrees[target] += len(sources)
+            else
+                self.graph.degrees[target] = len(sources)
+            # updates degrees and edges dictionaries for sources
             for src in sources:
+                if src not in graph.degrees:
+                    self.graph.degrees[src] = 0
                 if src not in graph.edges
-                    graph.edges[src] = [target]
+                    self.graph.edges[src] = [target]
                 else
-                    graph.edges[src].append(target)
+                    self.graph.edges[src].append(target)
             
-            # compilation line right after
-            actions[line[0]] = lines[line_num + 1]
+            # places compilation line in actions dictionary with target as key
+            self.actions[target] = lines[line_num + 1]
         
