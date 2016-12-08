08d1fd47d9dff0995b324739985e4dc9967b3ab9
Royce Branning <rbrannin@nd.edu>
2016-12-08 17:42:44 -0500

    added time-stamp tests class

--- test/test.py
+++ test/test.py
@@ -33,11 +33,16 @@ class TestTopologicalSort(unittest.TestCase):
         self.assertEqual(results, correct)
 
 
-class TestMakefile(unittest.TestCase):
+class TestMakefile1(unittest.TestCase):
  
     @classmethod
     def setUpClass(cls):
         os.chdir('test/test_1')
+    
+    @classmethod
+    def tearDownClass(self):
+        # returns to original directory after tests are run
+        os.chdir('../../')
 
     def test_output_make_all(self):
         mf = Makefile('Makefile', '', True)
@@ -58,3 +63,57 @@ class TestMakefile(unittest.TestCase):
         correct = ['g++ solution.cpp -g -Wall -std=gnu++11 -o solution',
                    'touch test.xyz']
         self.assertEqual(mf.output, correct)
+
+class TestMakeFile2(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        os.chdir('test/test_2')
+
+    @classmethod
+    def tearDownClass(self):
+        # returns to original directory after tests are run
+        os.chdir('../../')
+
+    def tearDown(self):
+        # removes files left after a method e.x. test_output_make_make
+        mf = Makefile('Makefile', 'clean', False)
+        mf.run_makefile()
+
+
+    def test_output_make_all(self):
+        mf = Makefile('Makefile', '', True)
+        mf.run_makefile()
+        correct = [ 'gcc -std=gnu99 -Wall -c -o hello.o hello.c',
+                    'gcc -std=gnu99 -Wall -c -o main.o main.c',
+                    'gcc -o hello main.o hello.o']
+        self.assertEqual(mf.output, correct)
+
+    def test_output_make_clean(self):
+        mf = Makefile('Makefile', 'clean', True)
+        mf.run_makefile()
+        correct = ['rm -f hello *.o']
+        self.assertEqual(mf.output, correct)
+
+    # checks that unchanged files are not recompiled
+    def test_output_make_make(self):
+        # mf needs to set just_print to false in order to test mf2
+        mf = Makefile('Makefile', '', False)
+        mf.run_makefile()
+        mf2 = Makefile('Makefile', '', True)
+        mf2.run_makefile()
+        correct = []
+        self.assertEqual(mf2.output, correct)
+
+
+    # checks that changed files are recompiled
+    def test_output_make_clean_make(self):
+        mf = Makefile('Makefile', '', False)
+        mf.run_makefile()
+        mf2 = Makefile('Makefile', 'clean', False)
+        mf2.run_makefile()
+        mf3 = Makefile('Makefile', '', True)
+        mf3.run_makefile()
+        correct = [ 'gcc -std=gnu99 -Wall -c -o hello.o hello.c',
+                    'gcc -std=gnu99 -Wall -c -o main.o main.c',
+                    'gcc -o hello main.o hello.o']
+        self.assertEqual(mf3.output, correct)
